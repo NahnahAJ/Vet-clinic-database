df4a30d100179903333a8a9b34569d107bc122df
nahnahAJ <nahnahahjuwah@gmail.com>
2022-12-06 12:15:46 +0000

    SQL used for creating data for the animals with their respective data types

--- schema.sql
+++ schema.sql
@@ -1,5 +1,11 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    name varchar(100)
+    name VARCHAR(250),
+    id INT GENERATED ALWAYS AS IDENTITY,
+    date_of_birth date,
+    escape_attempts INT,
+    neutered BIT,
+    weight_kg DECIMAL,
+    PRIMARY KEY(id)
 );
