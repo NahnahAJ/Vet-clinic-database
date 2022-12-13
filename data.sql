4b405672b2ef7e027ba39b6acc73bf6a528352b7
nahnahAJ <nahnahahjuwah@gmail.com>
2022-12-13 17:42:47 +0000

    Insert new information into the database

--- data.sql
+++ data.sql
@@ -181,3 +181,14 @@ INSERT INTO visits(animal_id, vets_id, date_of_visit) VALUES (10, 3, '2020-05-24
 
 -- Blossom visited William Tatcher on Jan 11th, 2021.
 INSERT INTO visits(animal_id, vets_id, date_of_visit) VALUES (10, 1, '2021-01-11');
+
+
+-- VET CLINIC DATABASE: DATABASE PERFORMANCE AUDIT
+
+-- This will add 3.594.280 visits considering you have 10 animals, 4 vets, and it will use around ~87.000 timestamps (~4min approx.)
+INSERT INTO visits (animal_id, vets_id, date_of_visit) SELECT * FROM (SELECT id FROM animals) animal_ids, (SELECT id FROM vets) vets_ids, generate_series('1980-01-01'::timestamp, '2021-01-01', '4 hours') visit_timestamp;
+
+-- This will add 2.500.000 owners with full_name = 'Owner <X>' and email = 'owner_<X>@email.com' (~2min approx.)
+insert into owners (full_name, email) select 'Owner ' || generate_series(1,2500000), 'owner_' || generate_series(1,2500000) || '@mail.com';
+
+-- explain analyze SELECT COUNT(*) FROM visits where animal_id = 4
